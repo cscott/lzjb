0e0ebc6bc854aa58f1633f474733825cad60a319
C. Scott Ananian <cscott@cscott.net>
2013-03-06 02:00:49 -0500

    Improvements to test suite.

--- test/IuppiterTest.js
+++ test/IuppiterTest.js
@@ -2,19 +2,21 @@ var assert = require('assert');
 var lzjb = require('../');
 var fs = require('fs');
 
-describe('Hello World!', function() {
+describe('Hello World!!!', function() {
     var s = "Hello World!!!Hello World!!!Hello World!!!Hello World!!!";
     for(var i = 0; i < 10; i++)
         s += s;
     s = new Buffer(s, 'utf8');
 
     var c = lzjb.compress(s);
+    c = new Buffer(c);
     it('should be smaller when compressed', function() {
         assert.ok(c.length < s.length, c);
     });
 
     var d = lzjb.decompress(c);
     d = new Buffer(d);
+
     it('should decompress to the original string', function() {
         assert.ok(d.toString('utf8') === s.toString('utf8'), d.toString('utf8'));
     });
